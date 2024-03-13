35430de4b129376e8296d6e19fbad765b86dce40
Ganesh1101 <ganeshofficial1101@gmail.com>
2024-03-13 14:29:26 +0530

    signin Screen Added

--- Client/Mobile/src/organisms/Authen/Signin.tsx
+++ Client/Mobile/src/organisms/Authen/Signin.tsx
@@ -1,8 +1,87 @@
-import { Text } from "react-native"
+import { Text,View, TextInput,Keyboard, Image, Switch, TouchableOpacity, Alert} from "react-native"
+import { styles } from "../../styles/styles";
+import { useState } from "react";
 
-const Signin = () => {
+const Signin = ({navigation}) => {
+    const[username,setUsername]=useState('');
+    const[password,setPassword]=useState('');
+    const[isRemember,setIsRemember]=useState(false);
+    const [errortext, setErrortext] = useState('');
+    const handleSubmitPress = () => {
+        if (!username) {
+            return Alert.alert('Invalid','Enter User Name');
+            
+        }
+        if (!password) {
+            return Alert.alert('Invalid','Enter Password')
+           
+        } 
+        navigation.navigate('Landing');
+    }
     return(
-    <Text>Signin</Text>
+    <View style={styles.mainContainer}>
+         <View style={{ alignItems: 'center' }}>
+                            <Image
+                                source={require('../../../assets/images/test_logo.png')}
+                                style={styles.logoimg}
+                            />
+                            <Text style={{ fontSize: 20 }}>Signin Here</Text>
+                        </View>
+        <View style={styles.SectionStyle}>
+                            <TextInput
+                                style={styles.inputStyle}
+                                onChangeText={(Username) =>
+                                    setUsername(Username)
+                                }
+                                placeholder="Enter Username" //dummy@abc.com
+                                placeholderTextColor="#8b9cb5"
+                                autoCapitalize="none"
+                                returnKeyType="next"
+                            />
+        </View>
+        <View style={styles.SectionStyle}>
+                            <TextInput
+                                style={styles.inputStyle}
+                                onChangeText={(UserPassword) =>
+                                    setPassword(UserPassword)
+                                }
+                                placeholder="Enter Password" //12345
+                                placeholderTextColor="#8b9cb5"
+                                keyboardType="default"
+                                onSubmitEditing={Keyboard.dismiss}
+                                secureTextEntry={true}
+                                returnKeyType="next"
+                            />
+        </View>
+        <View style={{ flexDirection: 'row', marginLeft: 10, marginRight: 10 }}>
+                            <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center' }}>
+                                <Switch trackColor={{false: '#767577', true: styles.appColor.color}} thumbColor={isRemember ? styles.appColor.color : '#f4f3f4'}
+                                    ios_backgroundColor="#3e3e3e" onValueChange={setIsRemember} value={isRemember}/>
+                                <Text style={styles.headerTextStyle}>Remember</Text>
+                            </View>
+                            <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center' }}>
+                                <Text
+                                    style={styles.headerTextStyle}
+                                    onPress={() => Alert.alert('Forgot Password')}>
+                                    Forgot Password
+                                </Text>
+                            </View>
+                        </View>
+                        <View style={{ alignItems: "center" }}>
+                            <TouchableOpacity
+                                style={styles.buttonStyle}
+                                activeOpacity={0.5}
+                                onPress={handleSubmitPress}>
+                                <Text style={styles.buttonTextStyle}>SIGNIN</Text>
+                            </TouchableOpacity>
+                        </View>
+
+                        <Text
+                            style={styles.pressableTextStyle}
+                            onPress={() => navigation.navigate('RegisterScreen')}>
+                            Don't have an account ? SIGNUP HERE
+                        </Text>
+    </View>
     );
 }
 
--- Client/Mobile/src/styles/styles.tsx
+++ Client/Mobile/src/styles/styles.tsx
@@ -5,7 +5,7 @@ const transparent = "transparent";
 const darkText = "#212121";
 const lightText = '#FAFAFA';
 const primaryColor = "#952323";
-const secondaryColor = "#A63120";
+const secondaryColor = "#F2E8C6";
 const fieldColor = '#A63120';
 const textColor = useColorScheme() === 'light' ? darkText : lightText;
 
@@ -29,6 +29,70 @@ const styles = StyleSheet.create({
         tintColor: primaryColor,
         overlayColor: transparent,
     },
-    
+    S_background:{
+        backgroundColor:secondaryColor,
+        height:'100%',
+        width:'100%'
+    },
+    mainContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: secondaryColor,
+        alignContent: 'center',
+        paddingLeft: 25,
+        paddingRight: 25,
+    },
+    inputStyle: {
+        flex: 1,
+        color: textColor,
+        paddingLeft: 15,
+        paddingRight: 15,
+        borderWidth: 1,
+        borderRadius: 30,
+        borderColor: primaryColor,
+        fontSize: 20,
+    },
+    SectionStyle: {
+        flexDirection: 'row',
+        height: 65,
+        marginTop: 20,
+        margin: 10,
+    },
+    headerTextStyle: {
+        color: textColor,
+        fontWeight: 'bold',
+        fontSize: 15,
+    },
+    logoimg: {
+        width: '70%',
+        height: 160,
+        resizeMode: 'contain',
+        margin: 30,
+    },
+    pressableTextStyle: {
+        color: textColor,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 14,
+        alignSelf: 'center',
+        padding: 10,
+    },
+    buttonStyle: {
+        backgroundColor: primaryColor,
+        borderWidth: 0,
+        color: '#FFFFFF',
+        borderColor: primaryColor,
+        height: 40,
+        width: 180,
+        alignItems: 'center',
+        borderRadius: 20,
+        marginTop: 20,
+        marginBottom: 25,
+    },
+    buttonTextStyle: {
+        color: '#FFFFFF',
+        paddingVertical: 10,
+        fontSize: 16,
+    },
 });
 export { styles };
