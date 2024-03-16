0967a6b7b435554bf1fcb0da506efef176c651c4
kgayathri007 <gayathrikatamgari@gmail.com>
2024-03-16 18:40:20 +0530

    login page added

--- Client/Web/oneKeyWeb/src/app/app.component.ts
+++ Client/Web/oneKeyWeb/src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
+import { RoleSelectionComponent } from './role-selection/role-selection.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,LoginComponent],
+  imports: [RouterOutlet,LoginComponent,RoleSelectionComponent,SignUpComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
