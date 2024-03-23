3f42da64f15eab8342fce1e15aa6ab0f4af8d80c
skayisha <ayisha7815@gmail.com>
2024-03-23 21:12:14 +0530

    constant base responses

--- Backend/helpers/constants.js
+++ Backend/helpers/constants.js
@@ -2,7 +2,12 @@ const BASE_RESPONSE_MESSAGES = {
     SUCCESS: {
         USER_REGISTERED: 'User registered successfully',
         ROLE_SELECTED: 'User role selected successfully',
-        LOGIN_SUCCESSFUL: 'Login successful'
+        LOGIN_SUCCESSFUL: 'Login successful',
+        PRODUCT_CREATE_SUCCESSFUL: 'Product created successfully',
+        PRODUCT_DELETE_SUCCESSFUL:'Product deleted successfully',
+        GET_ALL_PRODUCT_SUCCESSFUL: 'Product retrieved successfully',
+        PRODUCT_UPDATE_SUCCESSFUL:'Product updated successfully',
+        GET_PRODUCT_BY_ID_SUCCESSFUL: 'Product retrieved successfully',
     },
     ERROR: {
         ALL_FIELDS_REQUIRED: 'All fields are required',
@@ -10,7 +15,8 @@ const BASE_RESPONSE_MESSAGES = {
         USER_EXISTS: 'Username or email already exists',
         USER_NOT_FOUND: 'User not found',
         INVALID_ROLE: 'Invalid role',
-        WRONG_PASSWORD: 'Wrong password'
+        WRONG_PASSWORD: 'Wrong password',
+        PRODUCT_NOT_FOUND:'Product not found'
     }
 };
 
