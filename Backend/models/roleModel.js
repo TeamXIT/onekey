8547ad1b2d62672fcc4d6311cd2939f37f7a30b7
Rajasekhar Donkala <rajasekhar@groovejones.com>
2024-03-25 22:33:14 +0530

    added default roles

--- Backend/models/roleModel.js
+++ Backend/models/roleModel.js
@@ -10,5 +10,16 @@ const Role = sequelize.define("Roles", {
         allowNull: false,
         unique: true
     }
-}, { tableName: 'Roles', timestamps: false });
+}, {
+    tableName: 'Roles', timestamps: false,
+    hooks: {
+        afterSync: async () => {
+            // Create roles if they do not exist
+            await Role.findOrCreate({ where: { role_name: 'admin' }, defaults: { role_id: 1 } });
+            await Role.findOrCreate({ where: { role_name: 'agent' }, defaults: { role_id: 2 } });
+            await Role.findOrCreate({ where: { role_name: 'bpo' }, defaults: { role_id: 3 } });
+            // Add more roles as needed
+        }
+    }
+});
 module.exports = { Role };
