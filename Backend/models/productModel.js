a3a22671b6aab5f7f3692aee6be7ffa109caebf0
Ganesh Thirumalapudi <ganeshofficial1101@gmail.com>
2024-05-31 12:49:08 +0530

    projects controller added

--- Backend/models/productModel.js
+++ Backend/models/productModel.js
@@ -4,6 +4,11 @@ const { User } = require('./roleModel');
 
 
 const Product = sequelize.define("Products", {
+    projectId:{
+        type:Datatypes.INTEGER,
+        autoIncrement:true,
+        primaryKey:true,
+    },
     projectName: {
         type: Datatypes.STRING(100),
         allowNull: false,
@@ -20,88 +25,11 @@ const Product = sequelize.define("Products", {
         allowNull: false,
         required: true,
     },
-    propertyDetails: {
-        type: Datatypes.ARRAY([Datatypes.JSON]),
-        allowNull: false,
-        validate: {
-            isPropertyDetailsArray(value) {
-                if (!Array.isArray(value)) {
-                    throw new Error('Property details must be an array');
-                }
-                value.forEach(detail => {
-                    if (typeof detail.areaInSquareYards !== 'number' ||
-                        typeof detail.numberOfPlots !== 'number' ||
-                        typeof detail.price !== 'number') {
-                        throw new Error('Each property detail must contain numbers for areaInSquareYards, numberOfPlots, and price');
-                    }
-                });
-            }
-        }
-    },
-    propertyDescription: {
-        type: Datatypes.TEXT,
-        allowNull: false,
-        required: false,
-    },
-    propertyAmenities: {
-        type: Datatypes.JSONB,
-        defaultValue: {
-            "Park": false,
-            "HMDA Approval": false,
-            "RERA Approved": false,
-            "CC Roads": false,
-            "Water Supply to each plot": false,
-            "Avenue Plantation": false,
-            "Electricity with Street Lights": false,
-            "Underground Drainage": false,
-            "Power Backup": false,
-            "CCTV": false,
-            "Compound Wall": false,
-            "DTCP Approved": false,
-            "Entrance Arch": false,
-            "BT Roads": false,
-            "Community Hall": false,
-            "Children's Play Area": false
-        },
-        allowNull: false
-    },
-    propertyHighlights: {
-        type: Datatypes.TEXT,
-        allowNull: false,
-        required: false,
-    },
-    propertyLocation: {
-        type: Datatypes.JSONB,
-        allowNull: false,
-        validate: {
-            isValidLocation(value) {
-                if (typeof value !== 'object') {
-                    throw new Error('Property location must be an object');
-                }
-                const requiredFields = ['address', 'state', 'city', 'town_village', 'pincode', 'google_maps_latitude', 'google_maps_longitude'];
-                for (const field of requiredFields) {
-                    if (!(field in value)) {
-                        throw new Error(`Missing required field '${field}' in property location`);
-                    }
-                }
-            }
-        }
-    },
-    images:{
-        type: Datatypes.ARRAY(Datatypes.STRING),
-        allowNull: false,
-        required: true,
-    },
-    websiteLink:{
-        type: Datatypes.STRING(100),
-        allowNull: false,
-        required: true,
-    },
     owner_id: {
         type: Datatypes.INTEGER,
         allowNull: false,
         references: {
-            model: User, // Referenced table name
+            model: User,
             key: 'user_id',
         }
     },
