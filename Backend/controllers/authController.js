5ea4c0880a08d1d10e6c2b0577d70f9beb2c68fa
skayisha <ayisha7815@gmail.com>
2024-06-11 10:31:32 +0530

    sign in with password added

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -61,33 +61,24 @@ const setPassword = async (req, res) => {
         if (!mobileNumber || !password || !confirmPassword) {
             return res.status(400).json(baseResponses.constantMessages.ALL_FIELDS_REQUIRED());
         }
-        const user = User.findOne({where:{mobileNumber:mobileNumber}});
-        if(!user) {
+        const user = User.findOne({ where: { mobileNumber: mobileNumber } });
+        if (!user) {
             return res.status(404).json(baseResponses.constantMessages.USER_NOT_FOUND());
         }
         const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
-        
+
         if (!passwordRegex.test(password)) {
             return res.status(400).json(baseResponses.constantMessages.PASSWORD_INVALID());
         }
-        if(password!=confirmPassword){
+        if (password != confirmPassword) {
             return res.status(400).json(baseResponses.constantMessages.PASSWORD_MISMATCH());
         }
-        const updatePassword = await User.update({password:password},{where:{mobileNumber:mobileNumber}});
+        const updatePassword = await User.update({ password: password }, { where: { mobileNumber: mobileNumber } });
         return res.status(200).json('updatePassword');
-    }catch (error){
+    } catch (error) {
         return res.status(500).json(baseResponses.error(error.message));
     }
 }
-// const signUp = async (req, res) => {
-//     try{
-//         const {mobileNumber} = req.body;
-//         if(!mobileNumber){
-//             return res.status(400).json(baseResponses.constantMessages.INVALID_MOBILE_NUMBER());
-//         }
-//         const {otp} =
-//     }
-// };
 
 
 const selectRole = async (req, res) => {
@@ -120,7 +111,7 @@ const selectRole = async (req, res) => {
             return res.status(404).json(baseResponses.constantMessages.INVALID_ROLE());
         }
         let userrole_id = existingRole.role_id
-        let user_id=user.user_id
+        let user_id = user.user_id
         //updating the role_id of user 
         const assignedRole = await User.update({ role_id: userrole_id }, { where: { mobileNumber: mobileNumber } });
         // let _secret = process.env.JWT_SECRET || 'rajasekhar-secret-key';
@@ -130,57 +121,49 @@ const selectRole = async (req, res) => {
     }
 }
 
-// const signIn = async (req, res) => {
-//     try {
-//         const {
-//             username,
-//             password
-//         } = req.body;
-//         if (!username || !password) {
-//             return res.status(400).json(baseResponses.constantMessages.ALL_FIELDS_REQUIRED());
-//         }
-//         if (username.length < 4) {
-//             return res.status(400).json(baseResponses.constantMessages.USERNAME_LENGTH_ERROR());
-//         }
-//         if (password.length < 8) {
-//             return res.status(400).json(baseResponses.constantMessages.PASSWORD_LENGTH_ERROR());
-//         }
+const signInWithPassword = async (req, res) => {
+    try {
+        const {
+            mobileNumber,
+            password
+        } = req.body;
+        if (!mobileNumber || !password) {
+            return res.status(400).json(baseResponses.constantMessages.ALL_FIELDS_REQUIRED());
+        }
+        const { valid, response, phoneNumber } = validatePhoneNumber(mobileNumber);
+        if (!valid) {
+            return res.status(400).json({ response });
+        }
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
 
-//         if (!/[A-Z]/.test(password)) {
-//             return res.status(400).json(baseResponses.constantMessages.PASSWORD_UPPERCASE_ERROR());
-//         }
+        if (!passwordRegex.test(password)) {
+            return res.status(400).json(baseResponses.constantMessages.PASSWORD_INVALID());
+        }
 
-//         if (!/[a-z]/.test(password)) {
-//             return res.status(400).json(baseResponses.constantMessages.PASSWORD_LOWERCASE_ERROR());
-//         }
+        const user = await User.findOne({
+            where: { mobileNumber: mobileNumber },
+        });
+        if (!user) {
+            return res.status(404).json(baseResponses.constantMessages.USER_NOT_FOUND());
+        }
+        if (user.role_id == null) {
+            return res.status(400).json(baseResponses.constantMessages.ROLE_NOT_FOUND_ERROR());
+        }
+        if (password !== user.password) {
+            return res.status(400).json(baseResponses.constantMessages.WRONG_PASSWORD());
+        }
+        let user_id = user.user_id;
+        let _secret = process.env.JWT_SECRET || 'rajasekhar-secret-key';
+        const token = jwt.sign({ mobileNumber, user_id, role: user.role_id }, _secret, { expiresIn: '1h' });
+        return res.status(200).json(baseResponses.constantMessages.LOGIN_SUCCESSFUL({ token, user_id }));
 
-//         if (!/\d/.test(password)) {
-//             return res.status(400).json(baseResponses.constantMessages.PASSWORD_NUMBER_ERROR());
-//         }
+    } catch (error) {
+        res.status(500).json(baseResponses.error(error.message));
+    }
+}
 
-//         if (!/[^A-Za-z0-9]/.test(password)) {
-//             return res.status(400).json(baseResponses.constantMessages.PASSWORD_SYMBOL_ERROR());
-//         }
-//         const user = await User.findOne({
-//             where: { username },
-//         });
-//         if (!user) {
-//             return res.status(404).json(baseResponses.constantMessages.USER_NOT_FOUND());
-//         }
-//         if (user.role_id==null) {
-//             return res.status(400).json(baseResponses.constantMessages.ROLE_NOT_FOUND_ERROR());
-//         }
-//         if (password !== user.password) {
-//             return res.status(400).json(baseResponses.constantMessages.WRONG_PASSWORD());
-//         }
-//         let user_id=user.user_id;
-//         let _secret = process.env.JWT_SECRET || 'rajasekhar-secret-key';
-//         const token = jwt.sign({ username, user_id, role: user.role_id }, _secret, { expiresIn: '1h' });
-//         return res.status(200).json(baseResponses.constantMessages.LOGIN_SUCCESSFUL({ token, user_id }));
+const signInWithOtp = async (req, res) => {
 
-//     } catch (error) {
-//         res.status(500).json(baseResponses.error(error.message));
-//     }
-// }
+}
 
-module.exports = { sendOtp, otpVerification, setPassword,selectRole};
+module.exports = { sendOtp, otpVerification, setPassword, selectRole,signInWithPassword };
--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { sendOtp, otpVerification, setPassword, selectRole, } = require('../controllers/authController');
+const { sendOtp, otpVerification, setPassword, selectRole, signInWithPassword, } = require('../controllers/authController');
 const router = express.Router();
 router.post('/sign-up/sendOtp', sendOtp);
 router.post('/sign-up/verifyOtp', otpVerification);
 router.put('/sign-up/set-password', setPassword);
 router.put('/select-role', selectRole);
+router.post('/sign-in',signInWithPassword)
 // router.post('/sign-in', signIn);
 
 module.exports = router;
