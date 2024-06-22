eeb7ae7db26990c2da4be5c680d1234aa2765d87
Ganesh Thirumalapudi <ganeshofficial1101@gmail.com>
2024-06-22 13:24:39 +0530

    map

--- Client/Mobile/src/organisms/Landing/DynamicForm.tsx
+++ Client/Mobile/src/organisms/Landing/DynamicForm.tsx
@@ -1,5 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, TextInput, Button, ScrollView, StyleSheet, Platform, Alert, TouchableOpacity, Image } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  ScrollView,
+  StyleSheet,
+  Platform,
+  Alert,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../../reducers/store';
 import { getPropertyType } from '../../reducers/Product/productSlice';
@@ -10,6 +21,10 @@ import Geolocation from 'react-native-geolocation-service';
 import { request, PERMISSIONS } from 'react-native-permissions';
 import LinearGradient from 'react-native-linear-gradient';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import DocumentPicker from 'react-native-document-picker';
+import { RadioButton } from 'react-native-paper';
+import MapView, { Marker } from 'react-native-maps';
+
 const DynamicForm = ({ route }) => {
   const { propertyType } = route.params;
   const dispatch = useDispatch();
@@ -18,6 +33,11 @@ const DynamicForm = ({ route }) => {
   const [showDatePicker, setShowDatePicker] = useState(false);
   const [currentDateField, setCurrentDateField] = useState('');
   const [filteredCities, setFilteredCities] = useState([]);
+  const [mapModalVisible, setMapModalVisible] = useState(false);
+  const [selectedLocation, setSelectedLocation] = useState({
+    latitude: 37.78825,
+    longitude: -122.4324,
+  });
 
   useEffect(() => {
     dispatch(getPropertyType(propertyType));
@@ -28,10 +48,14 @@ const DynamicForm = ({ route }) => {
 
     const parseData = (data) => {
       if (data) {
-        data.forEach(item => {
+        data.forEach((item) => {
           if (item.input_type === 'checkbox') {
             initialState[item.name] = item.value || false;
-          } else if (['text_box', 'dropdown', 'radio_button', 'date'].includes(item.input_type)) {
+          } else if (
+            ['text_box', 'dropdown', 'radio_button', 'date'].includes(
+              item.input_type,
+            )
+          ) {
             initialState[item.name] = '';
           } else if (item.input_type === 'file') {
             initialState[item.name] = null;
@@ -48,22 +72,47 @@ const DynamicForm = ({ route }) => {
     setFormState(initialState);
   }, [formData]);
 
+  const handleFileUpload = async (fieldName) => {
+    try {
+      const res = await DocumentPicker.pick({
+        type: [DocumentPicker.types.allFiles],
+      });
+      const { uri, name, size, type } = res[0];
+      // Update formState with file information
+      setFormState((prevState) => ({
+        ...prevState,
+        [fieldName]: { uri, name, size, type },
+      }));
+    } catch (err) {
+      if (DocumentPicker.isCancel(err)) {
+        console.log('User cancelled the file picker');
+      } else {
+        console.log('Error picking file:', err);
+      }
+    }
+  };
   const handleChange = (name, value, inputType) => {
-    setFormState(prevState => ({
+    setFormState((prevState) => ({
       ...prevState,
       [name]: inputType === 'checkbox' ? value : value,
     }));
 
     if (name === 'state') {
-      const selectedStateId = formData.location.find(item => item.name === 'state')?.options.find(option => option.value === value)?.id;
-      const cities = formData.location.find(item => item.name === 'city')?.options.filter(option => option.parent_id === selectedStateId) || [];
+      const selectedStateId = formData.location
+        ?.find((item) => item.name === 'state')
+        ?.options.find((option) => option.value === value)?.id;
+      const cities =
+        formData.location
+          ?.find((item) => item.name === 'city')
+          ?.options.filter((option) => option.parent_id === selectedStateId) || [];
       setFilteredCities(cities);
-      setFormState(prevState => ({
+      setFormState((prevState) => ({
         ...prevState,
         city: '',
       }));
     }
   };
+
 
   const handleSubmit = () => {
     console.log(formState);
@@ -72,7 +121,9 @@ const DynamicForm = ({ route }) => {
   const requestLocationPermission = async () => {
     try {
       const granted = await request(
-        Platform.OS === 'ios' ? PERMISSIONS.IOS.LOCATION_WHEN_IN_USE : PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+        Platform.OS === 'ios'
+          ? PERMISSIONS.IOS.LOCATION_WHEN_IN_USE
+          : PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
       );
       return granted === 'granted';
     } catch (err) {
@@ -84,18 +135,18 @@ const DynamicForm = ({ route }) => {
   const captureLocation = async () => {
     const hasPermission = await requestLocationPermission();
     if (!hasPermission) {
-      Alert.alert('Permission Denied', 'Location permission is required to capture your location.');
+      Alert.alert(
+        'Permission Denied',
+        'Location permission is required to capture your location.'
+      );
       return;
     }
 
     Geolocation.getCurrentPosition(
       (position) => {
         const { latitude, longitude } = position.coords;
-        setFormState((prevState) => ({
-          ...prevState,
-          latitude: latitude.toString(),
-          longitude: longitude.toString(),
-        }));
+        setSelectedLocation({ latitude, longitude });
+        setMapModalVisible(true);
       },
       (error) => {
         Alert.alert('Error', 'Unable to retrieve your location.');
@@ -105,6 +156,14 @@ const DynamicForm = ({ route }) => {
     );
   };
 
+  const confirmLocation = () => {
+    setFormState((prevState) => ({
+      ...prevState,
+      latitude: selectedLocation.latitude.toString(),
+      longitude: selectedLocation.longitude.toString(),
+    }));
+    setMapModalVisible(false);
+  };
   const renderInputField = (item) => {
     if (!formData) {
       return null;
@@ -116,7 +175,9 @@ const DynamicForm = ({ route }) => {
           <View key={item.id} style={styles.checkboxContainer}>
             <CheckBox
               isChecked={formState[item.name]}
-              onClick={() => handleChange(item.name, !formState[item.name], item.input_type)}
+              onClick={() =>
+                handleChange(item.name, !formState[item.name], item.input_type)
+              }
               checkBoxColor="white"
               style={styles.checkbox}
             />
@@ -131,7 +192,9 @@ const DynamicForm = ({ route }) => {
             <TextInput
               style={styles.textInput}
               value={formState[item.name]}
-              onChangeText={(value) => handleChange(item.name, value, item.input_type)}
+              onChangeText={(value) =>
+                handleChange(item.name, value, item.input_type)
+              }
             />
           </View>
         );
@@ -141,17 +204,26 @@ const DynamicForm = ({ route }) => {
             <Text style={[styles.label, { color: 'white' }]}>{item.name}</Text>
             <Picker
               selectedValue={formState[item.name]}
-              onValueChange={(value) => handleChange(item.name, value, item.input_type)}
+              onValueChange={(value) =>
+                handleChange(item.name, value, item.input_type)
+              }
               style={styles.picker}
-              itemStyle={styles.pickerItem}
-            >
+              itemStyle={styles.pickerItem}>
               {item.name === 'city' && filteredCities.length > 0
-                ? filteredCities.map(option => (
-                  <Picker.Item key={option.id} label={option.name} value={option.name} />
-                ))
-                : item.options.map(option => (
-                  <Picker.Item key={option.id} label={option.value} value={option.value} />
-                ))}
+                ? filteredCities.map((option) => (
+                    <Picker.Item
+                      key={option.id}
+                      label={option.name}
+                      value={option.name}
+                    />
+                  ))
+                : item.options.map((option) => (
+                    <Picker.Item
+                      key={option.id}
+                      label={option.value}
+                      value={option.value}
+                    />
+                  ))}
             </Picker>
           </View>
         );
@@ -159,25 +231,60 @@ const DynamicForm = ({ route }) => {
         return (
           <View key={item.id} style={{ marginBottom: 10 }}>
             <Text style={[styles.label, { color: 'white' }]}>{item.name}</Text>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              {item.options.map((option) => (
+                <View key={option.id} style={styles.radioButtonContainer}>
+                  <RadioButton
+                    value={option.value}
+                    status={
+                      formState[item.name] === option.value ? 'checked' : 'unchecked'
+                    }
+                    onPress={() =>
+                      handleChange(item.name, option.value, item.input_type)
+                    }
+                    color="#008080"
+                  />
+                  <Text style={styles.radioButtonLabel}>
+                    {option.value}
+                  </Text>
+                </View>
+              ))}
+            </View>
           </View>
-        );
+        )
       case 'date':
         return (
-          <View key={item.id} style={{ marginBottom: 10, flexDirection: 'row', alignItems: 'center' }}>
-            <Text style={[styles.label, { color: 'white', marginRight: 10 }]}>{item.name}</Text>
+          <View
+            key={item.id}
+            style={{
+              marginBottom: 10,
+              flexDirection: 'row',
+              alignItems: 'center',
+            }}
+          >
+            <Text style={[styles.label, { color: 'white', marginRight: 10 }]}>
+              {item.name}
+            </Text>
             <TextInput
               style={[styles.textInput, { flex: 1, width: '60%' }]}
               value={formState[item.name]}
-              onChangeText={(value) => handleChange(item.name, value, item.input_type)}
+              onChangeText={(value) =>
+                handleChange(item.name, value, item.input_type)
+              }
               placeholder="Select Date"
               placeholderTextColor="gray"
               editable={false}
             />
-            <TouchableOpacity onPress={() => {
-              setShowDatePicker(true);
-              setCurrentDateField(item.name);
-            }}>
-              <Image source={require('../../images/calendar.png')} style={styles.calendarIcon} />
+            <TouchableOpacity
+              onPress={() => {
+                setShowDatePicker(true);
+                setCurrentDateField(item.name);
+              }}
+            >
+              <Image
+                source={require('../../images/calendar.png')}
+                style={styles.calendarIcon}
+              />
             </TouchableOpacity>
             {showDatePicker && currentDateField === item.name && (
               <DateTimePicker
@@ -187,18 +294,30 @@ const DynamicForm = ({ route }) => {
                 onChange={(event, date) => {
                   setShowDatePicker(false);
                   if (date) {
-                    handleChange(item.name, date.toISOString().split('T')[0], item.input_type);
+                    handleChange(
+                      item.name,
+                      date.toISOString().split('T')[0],
+                      item.input_type,
+                    );
                   }
                 }}
               />
             )}
           </View>
-        );
+        )
       case 'file':
         return (
           <View key={item.id} style={{ marginBottom: 10 }}>
-            <Text style={[styles.label, { color: 'white' }]}>{item.name}</Text>
-            <Button title="Upload File" onPress={() => { }} />
+            <Text style={{ color: 'white' }}>{item.name}</Text>
+            <Button
+              title="Upload File"
+              onPress={() => handleFileUpload(item.name)}
+            />
+            {formState[item.name] && (
+              <Text style={{ color: 'white', marginTop: 10 }}>
+                File selected: {formState[item.name].name}
+              </Text>
+            )}
           </View>
         );
       default:
@@ -214,10 +333,7 @@ const DynamicForm = ({ route }) => {
       {formData?.location?.map(renderInputField)}
       <View style={{ marginBottom: 20 }}>
         <TouchableOpacity onPress={captureLocation}>
-          <LinearGradient
-            colors={['#888693', '#35314A']}
-            style={styles.button}
-          >
+          <LinearGradient colors={['#888693', '#35314A']} style={styles.button}>
             <Text style={styles.buttonText}>Capture Location</Text>
           </LinearGradient>
         </TouchableOpacity>
@@ -228,10 +344,7 @@ const DynamicForm = ({ route }) => {
       {formData?.properties?.map(renderInputField)}
       <View style={{ marginBottom: 20 }}>
         <TouchableOpacity onPress={handleSubmit}>
-          <LinearGradient
-            colors={['#888693', '#35314A']}
-            style={styles.button}
-          >
+          <LinearGradient colors={['#888693', '#35314A']} style={styles.button}>
             <Text style={styles.buttonText}>Submit</Text>
           </LinearGradient>
         </TouchableOpacity>
@@ -303,9 +416,20 @@ const styles = StyleSheet.create({
   calendarIcon: {
     width: 30,
     height: 30,
-    tintColor: 'white',  // Adjust this if you want to change the icon color
+    tintColor: 'white', // Adjust this if you want to change the icon color
     marginLeft: 10,
+  },
+  radioButtonContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 5,
+  },
+  radioButtonLabel: {
+    marginLeft: 10,
+    fontSize: 14,
+    color: 'white',
   },
 });
 
 export default DynamicForm;
+
